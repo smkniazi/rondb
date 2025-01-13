334fbbe70b87a5d2d57ef497abe32f31ce97a7d7
Salman Niazi <salman@logicalclocks.com>
2025-01-13 15:17:05 +0100

    Proper fixes base on value types

--- storage/ndb/rest-server/rest-api-server/internal/handlers/feature_store/util.go
+++ storage/ndb/rest-server/rest-api-server/internal/handlers/feature_store/util.go
@@ -35,15 +35,7 @@ func DeserialiseComplexFeature(value *json.RawMessage, schema *avro.Schema) (*in
 		return nil, err
 	}
 
-	fmt.Printf("Schema %s\n", (*schema).String())
-	fmt.Printf("Deserialized Avro -> %v\n", avroDeserialized)
-
-	// by, err := json.MarshalIndent(avroDeserialized, "", " ")
-	// fmt.Println("Avro %s\n", string(by))
 	nativeJson := ConvertAvroToJson(avroDeserialized)
-	by, err := json.MarshalIndent(nativeJson, "", " ")
-	fmt.Printf("Deserialized Avro Json %s\n", string(by))
-
 	return &nativeJson, err
 }
 
@@ -65,49 +57,48 @@ func ConvertAvroToJson(o interface{}) interface{} {
 	var out interface{}
 	switch o.(type) {
 	case map[string]interface{}: // union or map
-		m := o.(map[string]interface{})
-		// fmt.Printf("Outer Case map\n")
-		for key := range m {
-			// sw := strings.Split(key, ".")[0]
-			// fmt.Printf("Inner case ConvertAvroToJson called %v, key %s,  switch %s\n", m, key, sw)
-			switch strings.Split(key, ".")[0] {
-
-			case "data":
-				result := make(map[string]interface{})
-				structValue := m[key].(map[string]interface{})
-				for structKey := range structValue {
-					result[structKey] = ConvertAvroToJson(structValue[structKey])
+		{
+			m := o.(map[string]interface{})
+			for key := range m {
+				switch m[key].(type) {
+				case map[string]interface{}:
+					{
+						result := make(map[string]interface{})
+						structValue := m[key].(map[string]interface{})
+						for structKey := range structValue {
+							result[structKey] = ConvertAvroToJson(structValue[structKey])
+						}
+						out = result
+					}
+				case []interface{}:
+					{
+						result := make([]interface{}, 0)
+						for _, item := range m[key].([]interface{}) {
+							itemJson := ConvertAvroToJson(item)
+							result = append(result, itemJson)
+						}
+						out = result
+					}
+				default:
+					{
+						out = ConvertAvroToJson(m[key])
+					}
 				}
-				out = result
-			case "sturct":
-				result := make(map[string]interface{})
-				structValue := m[key].(map[string]interface{})
-				for structKey := range structValue {
-					result[structKey] = ConvertAvroToJson(structValue[structKey])
-				}
-				out = result
-			case "array":
-				result := make([]interface{}, 0)
-				for _, item := range m[key].([]interface{}) {
-					itemJson := ConvertAvroToJson(item)
-					result = append(result, itemJson)
-				}
-				out = result
-			default:
-				// fmt.Printf("default case %v\n", m[key])
-				out = ConvertAvroToJson(m[key])
 			}
 		}
 	case []interface{}:
-		//fmt.Printf("Outer Case interface \n ")
-		result := make([]interface{}, 0)
-		for _, item := range o.([]interface{}) {
-			itemJson := ConvertAvroToJson(item)
-			result = append(result, itemJson)
+		{
+			result := make([]interface{}, 0)
+			for _, item := range o.([]interface{}) {
+				itemJson := ConvertAvroToJson(item)
+				result = append(result, itemJson)
+			}
+			out = result
 		}
-		out = result
 	default:
-		out = o
+		{
+			out = o
+		}
 	}
 	return out
 }
